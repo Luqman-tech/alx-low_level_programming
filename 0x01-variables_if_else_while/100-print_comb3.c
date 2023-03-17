2c346822a80fbe0d20fe2eb93c58b92807ae9997
Philip Kisaih <rexalchem69@gmail.com>
2023-03-17 16:53:27 -0700

    variable

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - main function
- *
- * Return always 0
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
-
 int main(void)
 {
 	int i;
 	int j;
 
-		for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; n < 10 ; i++)
+	{
+		for (i =  1; j < 10 ; j++)
+		{
+			if (i < j && i != j)
 			{
-			for (j = 1 ; j < 10 ; j++)
+				putchar(i + '0');
+				putchar(j + 'o');
+				if (i + j != 17)
 				{
-				if ( i < j && i != j)
-					{
-						putchar(i + '0');
-						putchar(j + '0');
-						if (i + j != 17)
-						{
-							putchar(',');
-							putchar(' ')
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
-			putchar('\n');
-			return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
