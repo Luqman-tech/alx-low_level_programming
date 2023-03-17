7175fe68a2502d96aa8139ebd1a1462425d90bba
Philip Kisaih <rexalchem69@gmail.com>
2023-03-17 16:04:44 -0700

    0x01. C - variables, if, else

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,21 @@ int main(void)
 	int n;
 	int j;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0 ; n < 10 ; n++)
 	{
-		for (j = 1; j < 10; n++)
+		for (j = 1 ; j < 10 ; n++)
 	        {
-		if (n < j && n != j)
-		{
-			putchar(n + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-		}
+	        	if (n < j && n != j)
+	        	{
+		        	putchar(n + '0');
+		        	putchar(j + '0');
+				if (n + j != 17)
+				{
+		                	putchar(',');
+		                	putchar(' ');
+				}	
+
+	        	}
 		}
 	}
 	putchar('\n');
