a5ca1067032c2c0cb12192e06e2b5669a7365b84
Philip Kisaih <rexalchem69@gmail.com>
2023-03-17 16:39:45 -0700

    0x01. C-variables, if, else

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0 (Success)
+ * main - main function
+ *
+ * Return always 0
  */
+
 int main(void)
 {
-	int n;
+	int i;
 	int j;
 
-	for (n = 0 ; n < 10 ; n++)
-	{
-		for (j = 1 ; j < 10 ; j++)
-	        {
-	        	if (n < j && n != j)
-	        	{
-		        	putchar(n + '0');
-		        	putchar(j + '0');
-				if (n + j != 17)
+		for (i = 0 ; i < 10 ; i++)
+			{
+			for (j = 1 ; j < 10 ; j++)
 				{
-		                	putchar(',');
-		                	putchar(' ');
-				}	
-
-	        	}
-		}
-	}
-	putchar('\n');
-	return (0);
+				if ( i < j && i != j)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						if (i + j != 17)
+						{
+							putchar(',');
+							putchar(' ')
+						}
+					}
+				}
+			}
+			putchar('\n');
+			return (0);
 }
+
